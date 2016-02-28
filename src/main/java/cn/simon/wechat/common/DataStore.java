49cc4ed9e520c7ebaecb56eb07999cb0a2f76598
Simon <shihd@fitech.com.cn>
2016-02-28 23:26:38 +0800

    增加userInfoMap

--- src/main/java/cn/simon/wechat/common/DataStore.java
+++ src/main/java/cn/simon/wechat/common/DataStore.java
@@ -1,5 +1,9 @@
 package cn.simon.wechat.common;
 
+import java.util.Map;
+
+import com.andson.model.UserInfo;
+
 import cn.simon.wechat.model.AccessToken;
 
 public class DataStore {
@@ -11,4 +15,6 @@ public class DataStore {
 	public final static String AppSecret = "a199958b060d6ff04a2f9dc54cad9b10";
 
 	public final static String Country_China = "CHN";
+
+	public static Map<String, UserInfo> userInfoMap;
 }
